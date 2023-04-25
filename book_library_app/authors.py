269976020f530181d8e85f31b7df4b32c80b307a
Marcin Kopański <65133294+marcinkkrk@users.noreply.github.com>
2023-04-26 00:06:30 +0200

    Methods updated

--- book_library_app/authors.py
+++ book_library_app/authors.py
@@ -2,6 +2,7 @@ from flask import jsonify
 from webargs.flaskparser import use_args
 from book_library_app import app, db
 from book_library_app.models import Author, AuthorSchema, author_schema
+from book_library_app.utils import validate_json_content_type
 
 
 @app.route('/api/v1/authors', methods=['GET'])
@@ -26,7 +27,8 @@ def get_author(author_id: int):
 
 
 @app.route('/api/v1/authors', methods=['POST'])
-@use_args(author_schema)
+@validate_json_content_type
+@use_args(author_schema, error_status_code=400)
 def create_author(args: dict):
     author = Author(**args)
 
@@ -41,15 +43,29 @@ def create_author(args: dict):
 
 
 @app.route('/api/v1/authors/<int:author_id>', methods=['PUT'])
-def update_author(author_id: int):
+@validate_json_content_type
+@use_args(author_schema, error_status_code=400)
+def update_author(args: dict, author_id: int):
+    author = Author.query.get_or_404(author_id, description=f"Author with id {author_id} not found")
+
+    author.first_name = args['first_name']
+    author.last_name = args['last_name']
+    author.birth_date = args['birth_date']
+
+    db.session.commit()
+
     return jsonify({
         'success': True,
-        'data': f'Author with id {author_id} has been updated'
+        'data': author_schema.dump(author)
     })
 
 
 @app.route('/api/v1/authors/<int:author_id>', methods=['DELETE'])
 def delete_author(author_id: int):
+    author = Author.query.get_or_404(author_id, description=f"Author with id {author_id} not found")
+
+    db.session.delete(author)
+    db.session.commit()
     return jsonify({
         'success': True,
         'data': f'Author with id {author_id} has been deleted'
