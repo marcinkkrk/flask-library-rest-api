9ae0d7da82552f4d3c0debb40de9cba6d246b9a2
Marcin Kopański <65133294+marcinkkrk@users.noreply.github.com>
2023-04-27 23:22:45 +0200

    updated models and functions, added filters

--- book_library_app/authors.py
+++ book_library_app/authors.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, request
 from webargs.flaskparser import use_args
 from book_library_app import app, db
 from book_library_app.models import Author, AuthorSchema, author_schema
@@ -7,8 +7,12 @@ from book_library_app.utils import validate_json_content_type
 
 @app.route('/api/v1/authors', methods=['GET'])
 def get_authors():
-    authors = Author.query.all()
-    author_schema = AuthorSchema(many=True)
+    query = Author.query
+    schema_args = Author.get_schema_args(request.args.get('fields'))
+    query = Author.apply_order(query, request.args.get('sort'))
+    query = Author.apply_filter(query, request.args)
+    authors = query.all()
+    author_schema = AuthorSchema(**schema_args)
 
     return jsonify({
         'success': True,
--- book_library_app/models.py
+++ book_library_app/models.py
@@ -1,6 +1,8 @@
+from flask_sqlalchemy import BaseQuery
 from book_library_app import db
 from datetime import datetime
 from marshmallow import Schema, fields, validate, validates, ValidationError
+from werkzeug.datastructures import ImmutableDict
 
 class Author(db.Model):   # class Author will be use to create table in db
     __tablename__ = 'authors'
@@ -13,6 +15,41 @@ class Author(db.Model):   # class Author will be use to create table in db
     def __repr__(self):
         return f'<{self.__class__.__name__}>: {self.first_name} {self.last_name}'
 
+
+    @staticmethod
+    def get_schema_args(fields: str) -> dict:
+        schema_args = {'many': True}
+        if fields:
+            schema_args['only'] = [field for field in fields.split(',') if field in Author.__table__.columns]
+        return schema_args
+
+    @staticmethod
+    def apply_order(query: BaseQuery, sort_keys: str) -> BaseQuery:
+        if sort_keys:
+            for key in sort_keys.split(','):
+                desc = False
+                if key.startswith('-'):
+                    key = key[1:]
+                    desc = True
+                column_attr = getattr(Author, key, None)
+                if column_attr is not None:
+                    query = query.order_by(column_attr.desc()) if desc else query.order_by(column_attr)
+        return query
+
+
+    @staticmethod
+    def apply_filter(query: BaseQuery, params: ImmutableDict) -> BaseQuery:
+        for param, value in params.items():
+            if param not in {'fields', 'sort'}:
+                column_attr = getattr(Author, param, None)
+                if column_attr is not None:
+                    if param == 'birth_date':
+                        try:
+                            value = datetime.strptime(value, '%d-%m-%Y').date()
+                        except ValueError:
+                            continue
+                    query = query.filter(column_attr == value)
+        return query
 
 class AuthorSchema(Schema):
     id = fields.Integer(dump_only=True)
